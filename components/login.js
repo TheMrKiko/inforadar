24d4d5b05de753ff53dd26a759508a48f07d655a
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2022-02-24 15:44:25 +0000

    Create login components with Google button

---
+++ components/login.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { withRouter } from 'next/router'
+import { GoogleLogin } from 'react-google-login';
+import { Typography, Modal, Button } from 'antd';
+
+import axios from 'axios';
+
+const { API_PATH } = process.env
+const { GOOGLE_CLIENT_ID } = process.env
+
+class LoginOptions extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.checkAlreadyLoggedIn();
+    }
+
+    componentDidUpdate() {
+        this.checkAlreadyLoggedIn();
+    }
+
+    checkAlreadyLoggedIn() {
+        // Redirect if authentication is done:
+        if (this.props.login.authenticated)
+            this.props.router.push({
+                pathname: '/'
+            })
+    }
+
+    loginSuccess(googleData) {
+        const formData = new FormData();
+        formData.set('id_token', googleData.tokenObj.id_token);
+
+        axios.post(`${API_PATH}/me`, formData, {
+            headers: { 'X-Requested-With': 'XmlHttpRequest' },
+        }).then(result => {
+            debugger
+            this.props.login.login(result.data);
+        }).catch(error => {
+            this.props.login.loginError(error);
+        });
+    };
+
+
+    loginFailure(d) {
+        this.props.login.loginError(d);
+    };
+
+
+    render() {
+        return (
+            <GoogleLogin
+                clientId={GOOGLE_CLIENT_ID}
+                buttonText={'Iniciar sessão com o Google'}
+                onSuccess={(gd) => this.loginSuccess(gd)}
+                onFailure={(d) => this.loginFailure(d)}
+                cookiePolicy={'single_host_origin'}
+                redirectUri={'postmessage'}
+                scope={'openid'}
+            />
+        );
+    }
+};
+
+
+export default withRouter(LoginOptions)
