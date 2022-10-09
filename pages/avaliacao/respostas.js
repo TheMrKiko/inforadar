027c29cc4ea12a8db0db9993b9f6489f7f456f8f
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2022-10-09 23:06:55 +0000

    Add replies table for each user

--- pages/avaliacao/respostas.js
+++ pages/avaliacao/respostas.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import { withRouter } from 'next/router'
 import Layout from '../../components/layout'
 import { Layout as AntLayout, Typography, Breadcrumb, Table } from 'antd'
 import { createError, errorType } from '../../helpers/error'
@@ -10,7 +11,7 @@ import utilStyles from '../../styles/utils.module.css'
 
 const { API_PATH } = process.env
 
-const columns = [
+const reportsColumns = [
     {
         title: 'Name',
         dataIndex: 'name',
@@ -63,10 +64,50 @@ const columns = [
         sorter: (a, b) => a.collection.length - b.collection.length,
         defaultFilteredValue: ['mint'],
     },
+    {
+        title: '',
+        key: 'details',
+        render: (_, record) => <Link
+            href={{
+                query: { userid: record.id },
+            }}>Ver Respostas</Link>,
+    },
 ];
 
-const Results = ({ login }) => {
+
+const repliesColumns = [
+    {
+        title: 'Artigo',
+        dataIndex: 'article_id',
+        key: 'article_id',
+        sorter: (a, b) => a.article_id - b.article_id,
+    },
+    {
+        title: 'Tempo demorado',
+        dataIndex: 'time_taken',
+        key: 'time_taken',
+        render: (time) => {
+            if (time === null)
+                return '-';
+            const secs = parseInt(time / 1000);
+            return `${Math.floor(secs / 60)}min ${secs % 60}s`;
+        },
+    },
+    {
+        title: 'Submetido em',
+        dataIndex: 'created_at',
+        key: 'created_at',
+        render: (d) => d.toLocaleString(),
+        sorter: (a, b) => a.created_at.getTime() - b.created_at.getTime(),
+        defaultSortOrder: 'ascend',
+    },
+];
+
+const Respostas = ({ login, router }) => {
     const [reports, setReports] = useState(null);
+    const [replies, setReplies] = useState(null);
+    const [currentUser, setCurrentUser] = useState(null);
+    const [currentUserInfo, setCurrentUserInfo] = useState(null);
 
     useEffect(() => {
         axios({
@@ -83,6 +124,42 @@ const Results = ({ login }) => {
         });
     }, []);
 
+    useEffect(() => {
+        const userid = parseInt(router.query.userid);
+        if (userid && !isNaN(userid))
+            setCurrentUser(userid);
+        else if (router.query.userid !== undefined)
+            router.push({
+                pathname: router.pathname,
+            });
+        else
+            setCurrentUser(null);
+    }, [router.query.userid]);
+
+    useEffect(() => {
+        setReplies(null);
+        if (currentUser)
+            axios({
+                method: 'get',
+                url: `${API_PATH}/user_replies?id=${currentUser}`,
+                headers: { 'content-type': 'application/json' }
+            }).then(result => {
+                setReplies(result.data.map((e, i) => ({ ...e, key: i, created_at: new Date(e.created_at) })));
+            }).catch(error => {
+                if (error.response && error.response.status === 401)
+                    login.loginError(createError(errorType.RELOGIN, error));
+                else
+                    login.loginError(createError(errorType.AUTHORIZE, error), false);
+            });
+    }, [currentUser]);
+
+    useEffect(() => {
+        if (!currentUser)
+            setCurrentUserInfo(null);
+        else if (reports)
+            setCurrentUserInfo(reports.find(r => r.id === currentUser));
+    }, [currentUser, reports]);
+
     return (
         <Layout current={'avaliacao'} login={login}>
             <Head>
@@ -94,7 +171,13 @@ const Results = ({ login }) => {
                         <Link href={'/avaliacao'}>Avaliação</Link>
                     </Breadcrumb.Item>
                     <Breadcrumb.Item>
-                        <Typography.Title level={1}>Respostas</Typography.Title>
+                        <Typography.Title level={1}>Respostas
+                            {!!currentUser &&
+                                <Typography.Text> de&nbsp;
+                                    {!!currentUserInfo &&
+                                        <Typography.Text underline>{currentUserInfo.name}</Typography.Text>}
+                                </Typography.Text>}
+                        </Typography.Title>
                     </Breadcrumb.Item>
                 </Breadcrumb>
                 <Typography.Paragraph>Consultar os dados dos anotadores remunerados na plataforma.</Typography.Paragraph>
@@ -103,13 +186,32 @@ const Results = ({ login }) => {
                         {!login.userData.admin ?
                             <Typography.Text type={'danger'}>Página apenas disponível para administradores. <Link href='/avaliacao'>Voltar atrás</Link>.</Typography.Text>
                             :
-                            <Table
-                                loading={!reports}
-                                columns={columns}
-                                dataSource={reports}
-                                scroll={{ x: true }}
-                            />
-                        }
+                            (!!currentUser ?
+                                <Table
+                                    key='replies'
+                                    loading={!replies}
+                                    columns={repliesColumns}
+                                    dataSource={replies}
+                                    scroll={{ x: true }}
+                                    pagination={{
+                                        defaultPageSize: 30,
+                                        showSizeChanger: true,
+                                        hideOnSinglePage: true,
+                                    }}
+                                />
+                                :
+                                <Table
+                                    key='reports'
+                                    loading={!reports}
+                                    columns={reportsColumns}
+                                    dataSource={reports}
+                                    scroll={{ x: true }}
+                                    pagination={{
+                                        defaultPageSize: 20,
+                                        hideOnSinglePage: true,
+                                    }}
+                                />
+                            )}
                     </Typography.Paragraph>
                     :
                     <Typography.Paragraph>
@@ -122,4 +224,4 @@ const Results = ({ login }) => {
 }
 
 
-export default Results
+export default withRouter(Respostas)
