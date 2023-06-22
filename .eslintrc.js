3a4eef62595e1364f6d0a229df47b223fcfb4165
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2023-06-22 00:45:55 +0000

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,38 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [{
-    files: ['*.ts', '*.tsx'],
-    plugins: ['react'],
-    extends: [
-      'airbnb',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier',
-    ],
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      project: './tsconfig.json',
-    },
-  }],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'airbnb',
+	],
+	overrides: [{
+		files: ['*.ts', '*.tsx'],
+		plugins: ['react'],
+		extends: [
+			'plugin:@typescript-eslint/recommended',
+			'plugin:react/recommended',
+			'airbnb-typescript',
+			'airbnb',
+		],
+		parserOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			project: './tsconfig.json',
+		},
+		rules: {
+			'@typescript-eslint/indent': ['error', 'tab'],
+			'no-tabs': ['off'],
+		},
+	}],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		'no-tabs': ['off'],
+		indent: ['error', 'tab'],
+	},
 };
