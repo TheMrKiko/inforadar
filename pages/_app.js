32cdc0661bd03872717dd68070e83264421da84a
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2025-04-12 16:44:41 +0000

    Initialize GA4

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
+import ReactGA from 'react-ga4'
 import withLogin from '../components/withlogin'
 
+const GA4 = process.env.GA4_TAG
+
+if (GA4) {
+	ReactGA.initialize(GA4)
+}
+
 function MyApp({ Component, pageProps, login }) {
-  return <Component {...pageProps} login={login} />
+	return <Component {...pageProps} login={login} />
 }
 
 export default withLogin(MyApp)
